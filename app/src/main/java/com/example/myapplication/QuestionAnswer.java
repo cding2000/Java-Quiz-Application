2e20f2bcace5c9910e0d00d4afc1d169e7a4372a
Chuqi Ding <82928785+cding2000@users.noreply.github.com>
2022-12-01 18:13:53 -0700

    python quiz app

---
+++ app/src/main/java/com/example/myapplication/QuestionAnswer.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Who developed Python Programming Language?",
+            "Which type of Programming does Python support??",
+            "Which of the following is the correct extension of the Python file??",
+            "Which of the following is used to define a block of code in Python language?",
+            "What does pip stand for python?"
+    };
+
+    public static String choices[][] = {
+            {"a) Wick van Rossum", "b) Rasmus Lerdorf", "c) Guido van Rossum", "d) Niene Stom"},
+            {"a) object-oriented programming", "b) structured programming", "c) functional programming", "d) all of the mentioned"},
+            {"a) .python", "b) .pl", "c) .py", "d) .p"},
+            {"a) Indentation", "b) Key", "c) Brackets", "d) All of the mentioned"},
+            {"a) Pip Installs Python",
+                    "b) Pip Installs Packages",
+                    "c) Preferred Installer Program",
+                    "d) All of the mentioned"}
+    };
+
+    public static String correctAnswers[] = {
+            "c) Guido van Rossum",
+            "d) all of the mentioned",
+            "c) .py",
+            "a) Indentation",
+            "c) Preferred Installer Program"
+    };
+
+}
